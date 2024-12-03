bfdcd85dd6126bc78cb06d36b3e009c7ee99dedf
Leo Kang <leo.kang@obigo.com>
2024-12-03 14:04:56 +0900

    wake up word가 제니아 으로 변경됨에 따라서 wake up word 제거 구문 수정

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -27,7 +27,7 @@ SAMPLE_RATE = 16_000
 dummy_stt = ['감사합니다.', '시청해주셔서 감사합니다.', 'MBC 뉴스 김성현입니다.',
              '다음 영상에서 만나요.', '다음 영상에서 만나요. 감사합니다.',
              '지금까지 뉴스 스토리였습니다.', '지금까지 뉴스 스토리였습니다. 날씨 알려줘']
-wuw_pattern = re.compile(r'(OK|오케이|오키|옥희|옥회의)+[,|.]?\s?(GEN|젠|겐|렌|잰|쟨|잼|쨈|전|쨍|쨘|제인|제안|이제)+[.]?', re.I)
+wuw_pattern = re.compile(r'(제니|자니|쟈니|쟤|쥐|쟤니|이제)+[,|.]?\s?(아|야|니야)+[.|?]?', re.I)
 
 logger = logging.getLogger('transcribe')
 logger.setLevel(logging.DEBUG)
